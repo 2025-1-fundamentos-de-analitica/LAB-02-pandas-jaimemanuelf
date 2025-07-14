bc52d082e4cc709bc947ba1a6bab703e83b77919
Jaime Manuel Figueroa <99105750+jaimemanuelf@users.noreply.github.com>
2025-07-13 23:22:12 -0500

    intento 1

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -6,11 +6,12 @@ librerias de pandas para resolver las preguntas.
 """
 
 
+import pandas as pd
 def pregunta_05():
     """
     Calcule el valor máximo de `c2` por cada letra en la columna `c1` del
     archivo `tbl0.tsv`.
-
+    
     Rta/
     c1
     A    9
@@ -20,3 +21,5 @@ def pregunta_05():
     E    9
     Name: c2, dtype: int64
     """
+    df = pd.read_csv('files/input/tbl0.tsv', sep='\t')
+    return df.groupby('c1')['c2'].max()
--- homework/pregunta_06.py
+++ homework/pregunta_06.py
@@ -4,14 +4,14 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
-
+import pandas as pd
 def pregunta_06():
     """
     Retorne una lista con los valores unicos de la columna `c4` del archivo
     `tbl1.csv` en mayusculas y ordenados alfabéticamente.
-
+    
     Rta/
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-
     """
+    df = pd.read_csv('files/input/tbl1.tsv', sep='\t')
+    return sorted([x.upper() for x in df['c4'].unique()])
