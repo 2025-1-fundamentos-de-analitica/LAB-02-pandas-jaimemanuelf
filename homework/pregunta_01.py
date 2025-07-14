424827661341e4f40eb4f0b2136bdc6e1ff479b9
Jaime Manuel Figueroa <99105750+jaimemanuelf@users.noreply.github.com>
2025-07-13 23:27:38 -0500

    intento 2

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,7 +3,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
 datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
-"""
+"""""
 
 
 import pandas as pd
